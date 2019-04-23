5abe52568f399f0e33ca55e1a1492dd93307ba64
Yang Liu <liuyang12@focusmedia.cn>
2019-04-23 18:37:06 +0800

    update

--- plot_series.py
+++ plot_series.py
@@ -4,19 +4,19 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 def f(x, y):
     z = x + y*1j
-    f = np.sin(z)
-    res = np.array(f, np.int64)
-    print(res.shape)
+    f = 1/ (1+np.power(z,2))
+    # res = np.array(f, np.int64)
     norm = abs(f)
-    print(norm.shape)
+    norm = np.clip(norm, 0, 8)
     return norm
 
-x = np.linspace(0., np.pi, 100)
-y = np.linspace(0., 1., 100)
+x = np.linspace(-3., 3., 50)
+y = np.linspace(-3., 3., 50)
 
 X, Y = np.meshgrid(x, y)
 Z = f(X, Y)
 ax = plt.axes(projection='3d')
+
 ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
                 cmap='viridis', edgecolor='none')
 ax.set_title('surface')
@@ -24,4 +24,7 @@ ax.set_title('surface')
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
+ax.set_xlim(-3., 3.)
+ax.set_ylim(-3., 3.)
+ax.set_zlim(0, 6)
 plt.show()
