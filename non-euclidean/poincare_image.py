d923d293de416997e3d3d0132834cc43063afd2f
unknown <foamliu@yeah.net>
2019-05-12 13:41:47 +0800

    update

--- non-euclidean/poincare_image.py
+++ non-euclidean/poincare_image.py
@@ -2,22 +2,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.image as mpimg
 
-filename = '../images/yangmi.jpg'
+filename = '../images/triangle.jpg'
 img = mpimg.imread(filename)
-img = img[:, :, 0]
+img = img/255.
 height, width = img.shape[:2]
 print(img.shape)
-u = np.arange(0, width)
-v = np.arange(0, height)
+print(np.max(img))
+u = np.linspace(-0.5, 0.5, width)
+v = np.linspace(1, 0, height)
 uu, vv = np.meshgrid(u, v)
+uu = uu.flatten()
+vv = vv.flatten()
 print(uu.shape)
 z0 = uu + 1j * vv
 z = (1j * z0 + 1) / (z0 + 1j)
-T = np.reshape(img, (height*width))
+# T = np.arctan2(uu, vv)
+img = np.reshape(img, (-1, 3))
+print(img.shape)
 
 plt.figure(figsize=(14, 6))
 plt.subplot(1, 2, 1)
-plt.scatter(uu, vv, c=T, s=10, lw=0, cmap=None)
+plt.scatter(uu, vv, c=img, s=10, lw=0, cmap=None)
 plt.title('real points')
 plt.xlabel('Re(z)')
 plt.ylabel('Im(z)')
@@ -25,10 +30,11 @@ plt.axis('equal')
 plt.grid(True)
 
 plt.subplot(1, 2, 2)
-plt.scatter(np.real(z), np.imag(z), c=T, s=10, lw=0, cmap='hsv')
+plt.scatter(np.real(z), np.imag(z), c=img, s=10, lw=0, cmap='hsv')
 plt.title('poincare disk')
 plt.xlabel('Re(z)')
 plt.ylabel('Im(z)')
 plt.axis('equal')
+
 plt.grid(True)
 plt.show()
