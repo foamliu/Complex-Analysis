e02b2b1847876efeab41a2d8b612f2274aa7563c
unknown <foamliu@yeah.net>
2019-05-26 08:37:20 +0800

    update

--- topology/winding_number.py
+++ topology/winding_number.py
@@ -2,8 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-
-
 def f(z):
     return (z - a) * (z - b) / (z - c)
 
@@ -15,11 +13,11 @@ c = 1.5 + 0.5 * 1j
 fa = f(a)
 fb = f(b)
 # fc = f(c)
-f1 = f(1)
+f1 = f(2)
 
 t = np.linspace(0, 2 * np.pi, 3600)
-uu = 1 * np.cos(t)
-vv = 1 * np.sin(t)
+uu = 2 * np.cos(t)
+vv = 2 * np.sin(t)
 z0 = uu + 1j * vv
 z = f(z0)
 
@@ -34,7 +32,7 @@ plt.ylabel('Im(z)')
 plt.axis('equal')
 plt.grid(True)
 plt.plot([np.real(a), np.real(b), np.real(c)], [np.imag(a), np.imag(b), np.imag(c)], 'ro')
-plt.plot([1], [0], 'bo')
+plt.plot([2], [0], 'bo')
 
 plt.text(np.real(a), np.imag(a), 'a', fontsize=12)
 plt.text(np.real(b), np.imag(b), 'b', fontsize=12)
@@ -42,7 +40,7 @@ plt.text(np.real(c), np.imag(c), 'c', fontsize=12)
 
 plt.subplot(1, 2, 2)
 plt.scatter(np.real(z), np.imag(z), c=T, s=3, lw=0, cmap='hsv')
-plt.title('image(w) plane')
+plt.title(' f(z)=(z-a)*(z-b)/(z-c)')
 plt.xlabel('Re(z)')
 plt.ylabel('Im(z)')
 plt.axis('equal')
